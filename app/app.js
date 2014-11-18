1ad774d1ae1fa1d21c6f3c3a1792c7efd148b0bc
Grace Lee <glee@definedlogic.com>
2014-11-18 10:55:41 -0500

    created basic folder structure, added cdns

---
+++ app/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mainApp = angular.module('mainApp', [
+	// 'ngRoute',
+	// 'controllers', // for controller.js
+	// other dependencies of this app, like controllers, services, etc.
+]);
+
+// Routes for mainApp
+/*mainApp.config(['$routeProvider', 
+	function($routeProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl: '',
+			controller: ''
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+	}
+]);*/
---
+++ controllers/controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var controllers = angular.module('controllers', []);
+
+controllers.controller('specificController', ['$scope', 
+	function($scope) {
+
+	}
+]);
